25ae71e43dcd511000178f992bef46e216a5eada
Solntselikaya <solntselikaya.sofia@yandex.ru>
2022-10-20 03:09:38 +0700

    Пробный SignInScreen
    Возникли некоторые проблемы с тем чтобы быть pixel-perfect.. не совсем понятно, как работает .height, .padding и прочие такие вещи..
    Нужно еще погуглить(спросить?) и изучить

--- app/src/main/java/com/example/movies_catalog/MainActivity.kt
+++ app/src/main/java/com/example/movies_catalog/MainActivity.kt
@@ -3,41 +3,145 @@ package com.example.movies_catalog
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Gray
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.InternalTextApi
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.movies_catalog.ui.theme.MoviesCatalogTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MoviesCatalogTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            val scrollState = rememberScrollState()
+            DefaultPreview();
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview(){
+    Column(modifier = Modifier.fillMaxSize()){
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Image(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(0.dp, 56.dp, 0.dp, 48.dp)
+                    .size(250.dp, 170.dp),
+                painter = painterResource(R.drawable.logo),
+                contentDescription = "Logo picture")
+            Login()
+            Spacer(modifier = Modifier.height(14.41.dp))
+            Password()
+        }
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally){
+            Button(
+                onClick = {/* TODO */},
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = colorResource(R.color.black),
+                    contentColor = colorResource(R.color.dark_red)),
+                border = BorderStroke(1.dp, colorResource(R.color.gray)),
+                modifier = Modifier.height(44.dp).padding(16.dp,0.dp,16.dp,4.dp).fillMaxWidth(),
+                shape = RoundedCornerShape(8.dp))
+            {
+                Text("Войти", fontSize = 16.sp)
+            }
+            Button(
+                onClick = {/* TODO */},
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = colorResource(R.color.black),
+                    contentColor = colorResource(R.color.dark_red)),
+                modifier = Modifier.padding(16.dp,8.dp,16.dp,16.dp).fillMaxWidth())
+            {
+                Text("Регистрация", fontSize = 16.sp)
             }
         }
     }
 }
 
+//@Preview(showBackground = true)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Login() {
+    var textValue by remember { mutableStateOf(TextFieldValue("")) }
+    OutlinedTextField(
+        value = textValue,
+        modifier = Modifier
+            .padding(16.dp, 0.dp)
+            .fillMaxWidth(),
+        onValueChange = {
+            textValue = it
+        },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            cursorColor = colorResource(R.color.dark_red),
+            focusedBorderColor = colorResource(R.color.dark_red),
+            unfocusedBorderColor = colorResource(R.color.gray),
+            textColor = colorResource(R.color.dark_red)
+        ),
+        placeholder = {
+            Text("Логин", color = colorResource(R.color.gray), fontSize = 14.sp) },
+        shape = RoundedCornerShape(8.dp)
+    )
 }
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    MoviesCatalogTheme {
-        Greeting("Android")
-    }
+fun Password() {
+    var textValue by remember { mutableStateOf(TextFieldValue("")) }
+    OutlinedTextField(
+        value = textValue,
+        modifier = Modifier
+            .padding(16.dp, 0.dp)
+            .fillMaxWidth(),
+        onValueChange = {
+            textValue = it
+        },
+        colors = TextFieldDefaults.outlinedTextFieldColors(
+            cursorColor = colorResource(R.color.dark_red),
+            focusedBorderColor = colorResource(R.color.dark_red),
+            unfocusedBorderColor = colorResource(R.color.gray),
+            textColor = colorResource(R.color.dark_red)
+        ),
+        placeholder = {
+            Text(
+                "Пароль",
+                color = colorResource(R.color.gray),
+                fontSize = 14.sp) },
+        shape = RoundedCornerShape(8.dp),
+
+        visualTransformation = PasswordVisualTransformation(),
+        keyboardOptions = KeyboardOptions(
+            //imeAction = ImeAction.Done,
+            keyboardType = KeyboardType.Password
+        )
+    )
 }
---
+++ app/src/main/java/com/example/movies_catalog/SignInScreen.kt
@@ -0,0 +1,15 @@
+package com.example.movies_catalog
+
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+
+@Preview(showBackground = true)
+@Composable
+fun SignInScreen() {
+    Scaffold() {
+        Text("Hello METANIT.COM", fontSize = 28.sp)
+    }
+}
